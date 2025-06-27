d31cb36368e08143daa34fd6192222b785bbd386
TJordan77 <tvon777@gmail.com>
2025-06-26 21:24:28 -0400

    Added Bilal's portion here as he was experiencing equipment issues

--- api/tasks.js
+++ api/tasks.js
@@ -13,7 +13,21 @@ router.get("/", (req, res) => {
 // Patch a task by id
 
 // Delete a task by id
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    Task.delete(id)
+    res.status(204).send();
+  } catch {
+    res.status(404).json({ message: 'Task missing' });
+  }
+});
 
 // Create a new task
+router.post("/", (req, res) => {
+  const { title, description, completed, userId } = req.body;
+  const newTask = Task.create({ title, description, completed, userId });
+  res.status(201).send(newTask);
+});
 
 module.exports = router;
